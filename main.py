5da54156e0135adfb8b130dd3c0de5a854525701
KWesterbeek <74435351+KWesterbeek@users.noreply.github.com>
2021-03-24 18:07:22 +0100

    Add files via upload
    - added comment to the clean_cache function, mainly the else statement.
    - moved all module imports to global
    - used on the argument cache_dir_path in cache_zip and added a line of code to make sure the working directory is the parent of the cache directory
    - added a try .. except to the find_password function
    - Called on the functions. Please update the paths to match the locations on your own computer!

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ __winc_id__ = "ae539110d03e49ea8738fd413ac44ba8"
 __human_name__ = "files"
 
 import os
+from zipfile import ZipFile
 
 cache_location = ".\\cache\\"
 
@@ -10,12 +11,15 @@ def clean_cache():
     try:
         # checks if cache exists and moves into it
         os.chdir(cache_location)
-    except:
+    except FileNotFoundError:
         # creates cache folder
         os.mkdir(cache_location)
     else:
         # The following will only be executed if cache already existed.
-        # creates list of files in cache and loops over the list to delete
+        # See https://www.w3schools.com/python/python_try_except.asp :
+        # the else will be executed only if try raises no errors. Which makes
+        # this a safe function: no other files will be accidentally deleted.
+        # Creates list of files in cache and loops over the list to delete
         cache_content = os.listdir()
         for file in cache_content:
             os.remove(file)
@@ -24,13 +28,22 @@ def clean_cache():
 
 
 def cache_zip(file_path, cache_dir_path):
-    from zipfile import ZipFile
-
+    # Because the assignment tells me to let the clean_cache function create
+    # the cache in the current directory, you can run into some errors if you
+    # specify a different cache location with the argument cache_dir_path.
+    # This could be prevented if you let the clean_cache function also
+    # take the cache_dir_path as argument.
+    # For now, a workaround is to set the working directory from within
+    # the function:
+    os.chdir(cache_dir_path.replace("\\cache\\", "\\"))
     # Clean cache or create it:
     clean_cache()
     # unzips contents of zipfile (file_path) into cache_dir_path
-    with ZipFile(file_path, "r") as zip_object:
-        zip_object.extractall(path=cache_location)
+    try:
+        with ZipFile(file_path, "r") as zip_object:
+            zip_object.extractall(path=cache_dir_path)
+    except FileNotFoundError:
+        print(f"zipfile {file_path} not found")
 
 
 def cached_files():
@@ -56,14 +69,24 @@ def cached_files():
 def find_password(cached_files):
     # loops over the cached files one by one and opens them
     for file in cached_files:
-        txt_file = open(file, "r")
-        # then loops over the lines in the opened file
-        for line in txt_file:
-            # if the line contains password it will slice from the space
-            # till the end of the line and put that in the variable 'password'
-            # it will close the file and return password thus ending the loops
-            if "password" in line:
-                password = line[line.find(" ") + 1 : -1]
-                txt_file.close
+        try:
+            txt_file = open(file, "r")
+            # then loops over the lines in the opened file
+            for line in txt_file:
+                # if the line contains password it will slice from the space
+                # till the end of the line and put that in the variable 'password'
+                # it will close the file and return password thus ending the loops
+                if "password" in line:
+                    password = line[line.find(" ") + 1 : -1]
+                    txt_file.close
+                    return password
+        except FileNotFoundError:
+            print(f"file {file} not found")
 
-                return password
+
+cache_zip(
+    "D:\\Programming\\Winc_Academy\\wincpy\\files\\data.zip",
+    "D:\\Programming\\Winc_Academy\\wincpy\\files\\cache\\",
+)
+
+print(find_password(cached_files()))
